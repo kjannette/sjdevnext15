e9075d3dff2f63565ce68a4ac58101b05366cf8d
Kenneth Jannette <sj@sjdev.co>
2025-02-21 13:07:43 -0500

    add anal

--- src/components/homeart.js
+++ src/components/homeart.js
@@ -1,18 +1,29 @@
 "use client";
 import styles from "../app/styles.module.css";
+import Head from "next/head";
 
 export default function Homeart() {
   return (
-    <div className={styles.homeContainer}>
-      <div className={styles.gridColumn}>
-        <div className={styles.imgBox}>
-          <img
-            src="https://res.cloudinary.com/dd8ewixm7/image/upload/v1739933776/seqTheFut_uepaah.gif"
-            className={styles.homeImg}
-            alt="An interesting cubist animation inspired by the Bauhaus school."
-          />
+    <main>
+      <Head>
+        <script
+          data-goatcounter="https://sjdev.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+          key="gc-1"
+        ></script>
+      </Head>
+      <div className={styles.homeContainer}>
+        <div className={styles.gridColumn}>
+          <div className={styles.imgBox}>
+            <img
+              src="https://res.cloudinary.com/dd8ewixm7/image/upload/v1739933776/seqTheFut_uepaah.gif"
+              className={styles.homeImg}
+              alt="An interesting cubist animation inspired by the Bauhaus school."
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
