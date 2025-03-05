c7c581d22f325a8f18c208b2d01342b92c3056ad
Kenneth Jannette <sj@sjdev.co>
2025-03-05 13:12:46 -0500

    more

--- src/firebase.js
+++ src/firebase.js
@@ -13,5 +13,35 @@ const firebaseConfig = {
   appId: "1:237955047524:web:7252afd767a32ee70f3f71",
 };
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} from "./secrets";
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+export const signInUser = async (email, password) => {
+  if (!email && !password) return;
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const userStateListener = (callback) => {
+  return onAuthStateChanged(auth, callback);
+};
+
+export const SignOutUser = async () => await signOut(auth);
+
+export { auth };
+export { db };
